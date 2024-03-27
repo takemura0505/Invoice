8ac786781a924ed9ee8e2ed0b53faca8ee7dca34
takemuraharuumi <takemuraharuumi@gmail.com>
2024-03-27 22:24:18 +0900

    Initial Commit

---
+++ Invoice/InvoiceApp.swift
@@ -0,0 +1,20 @@
+//
+//  InvoiceApp.swift
+//  Invoice
+//
+//  Created by 竹村はるうみ on 2024/03/27.
+//
+
+import SwiftUI
+
+@main
+struct InvoiceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
