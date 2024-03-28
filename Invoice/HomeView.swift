0c614503d0398339b07cf94501d2e186f3667f97
takemuraharuumi <takemuraharuumi@gmail.com>
2024-03-28 15:58:37 +0900

    HomeViewを追加

---
+++ Invoice/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  Invoice
+//
+//  Created by 竹村はるうみ on 2024/03/28.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
+
+extension HomeView {
+    
+    
+    
+}
