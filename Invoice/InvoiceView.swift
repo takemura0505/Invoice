7c9319560ae828b312ec2f68f26243f9c9be7adc
takemuraharuumi <takemuraharuumi@gmail.com>
2024-03-30 16:51:15 +0900

    InvoiceSheetを作成

--- Invoice/InvoiceView.swift
+++ Invoice/InvoiceView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct InvoiceView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            invoiceSheet
+        }
     }
+    
 }
 
 #Preview {
     InvoiceView()
+}
+
+extension InvoiceView {
+    
+    private var invoiceSheet: some View {
+        GeometryReader { geometry in
+            let width = geometry.size.width - 20
+            let height = width * sqrt(2)
+            
+            Rectangle()
+                .fill(Color.white)
+                .frame(width: width, height: height)
+                .shadow(radius: 2)
+                .border(Color.gray, width: 1)
+                .padding(.horizontal, 10)
+        }
+        .background(Color.gray.opacity(0.2))
+        .edgesIgnoringSafeArea(.all)
+    }
+    
 }
